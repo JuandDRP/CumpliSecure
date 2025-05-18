0efc39de4a78d06be5ba8aae1b57c84afa473ae0
Juan de Dios Rodriguez <135068211+JuandDRP@users.noreply.github.com>
2025-05-18 16:07:21 -0500

    Update credenciales.js

--- frontend/src/credenciales.js
+++ frontend/src/credenciales.js
@@ -2,11 +2,11 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8dSyLSNxRlvhX8PsTQbNEK0ie6XaXn5g",
+  apiKey: "",
   authDomain: "login-edya2.firebaseapp.com",
   projectId: "login-edya2",
   storageBucket: "login-edya2.appspot.com",
-  messagingSenderId: "89246934117",
+  messagingSenderId: "",
   appId: "1:89246934117:web:dff88b4c669c053289128c"
 };
 
