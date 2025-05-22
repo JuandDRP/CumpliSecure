a1544ed6cc4e0c512df89f7b1392c7f79766d967
Juan de Dios Rodriguez <135068211+JuandDRP@users.noreply.github.com>
2025-05-21 21:53:50 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = 'https://cumplisecure-db.onrender.com';
+const uri = 'mongodb+srv://juanderodriguez:Blu742cBz7rWWUd3@db.swjciap.mongodb.net/';
 const client = new MongoClient(uri);
 let db;
 
